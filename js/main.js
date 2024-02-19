d83f5eb50b36fe3bfa4abc517dd27a17286530e3
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-02-20 04:12:24 +0500

    imtihon

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let modeBtn = document.getElementById("light-dark");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
