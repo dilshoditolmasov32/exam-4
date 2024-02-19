4a3ce298b382367e7a52ec7ccc96498c0ead8feb
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-02-20 04:56:30 +0500

    uzgartirildi

--- js/main.js
+++ js/main.js
@@ -20,3 +20,18 @@ function toggleBacktop() {
     backtop.classList.remove("backtop-show");
   }
 }
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
